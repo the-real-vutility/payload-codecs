0b9a7399bc2cb743e88555d71768d65435061803
Brent Mellor <137229810+brentmellor@users.noreply.github.com>
2025-02-11 16:11:36 -0700

    New voltdrop packets, phase angle, signed active energy and diagnostic flag. (#12)

--- voltdrop_direct/aws-iot-core/readme.md
+++ voltdrop_direct/aws-iot-core/readme.md
@@ -21,45 +21,77 @@ Once again, please ensure that only Vutility VoltDrop data is invoking this Lamb
 
 ## `decodeUplink`
 
-There are four standard packet types that the Voltdrop direct can transmit. These deliver
+These are the standard packet types that the Voltdrop direct can transmit. These deliver
 various data measurements that can be returned in a json object. Most values are averaged
 over the interval since the specific packet was last transmitted.
 
 The diagnostic packets are sent under specific conditions and detailed descriptions of their content are documented in a later section.
 
-### Phase Voltage and Power Factor (Packet #40)
+### Consolidated Phase Voltage, Phase Angle, Amperage and Max Amperage (Packet #38)
+**NOTE:** The separate smaller packets are recommended for general use. Under low data rate or high spreading factor conditions this packet may fail to send due to LoRaWAN airtime restrictions. Certain LoRaWAN regions (i.e. EU868, AS923) and many public networks impose more stringent restrictions that necessitate longer transmit intervals.
 | Name | Description | Units |
 | --- | --- | :---: |
-| `voltageL1` | Average RMS voltage on Phase A (L1) since last transmit | V |
-| `voltageL2` | Average RMS voltage on Phase B (L2) since last transmit| V |
-| `voltageL3` | Average RMS voltage on Phase C (L3) since last transmit| V |
-| `powerFactorL1` | Percentage power factor of Phase A (L1) | % |
-| `powerFactorL2` | Percentage power factor of Phase A (L1) | % |
-| `powerFactorL3` | Percentage power factor of Phase A (L1) | % |
+| `voltageL1` | Average RMS voltage on Phase A (L1) | V |
+| `voltageL2` | Average RMS voltage on Phase B (L2) | V |
+| `voltageL3` | Average RMS voltage on Phase C (L3) | V |
+| `phaseAngleL1` | Average voltage/current angle offset of Phase A (L1) | 0-360° |
+| `phaseAngleL2` | Average voltage/current angle offset of Phase B (L2) | 0-360° |
+| `phaseAngleL3` | Average voltage/current angle offset of Phase C (L3) | 0-360° |
+| `capacitorVoltage` | The capacitor voltage at time of transmit. | V |
+| `currentL1` | Average RMS amps on Phase A (L1) | A |
+| `currentL2` | Average RMS amps on Phase B (L2) | A |
+| `currentL3` | Average RMS amps on Phase C (L3) | A |
+| `maxCurrentL1` | Maximum amps on Phase A (L1) | A |
+| `maxCurrentL2` | Maximum amps on Phase B (L2) | A |
+| `maxCurrentL3` | Maximum amps on Phase C (L3) | A |
+| `temperatureCelsius` | The temperature at time of transmit. | °C |
+
+### Phase Voltage and Phase Angle (Packet #39)
+| Name | Description | Units |
+| --- | --- | :---: |
+| `voltageL1` | Average RMS voltage on Phase A (L1) | V |
+| `voltageL2` | Average RMS voltage on Phase B (L2) | V |
+| `voltageL3` | Average RMS voltage on Phase C (L3) | V |
+| `phaseAngleL1` | Average voltage/current angle offset of Phase A (L1) | 0-360° |
+| `phaseAngleL2` | Average voltage/current angle offset of Phase B (L2) | 0-360° |
+| `phaseAngleL3` | Average voltage/current angle offset of Phase C (L3) | 0-360° |
+| `capacitorVoltage` | The capacitor voltage at time of transmit. | V |
+
+### Phase Voltage and IEC Power Factor (Packet #40)
+| Name | Description | Units |
+| --- | --- | :---: |
+| `voltageL1` | Average RMS voltage on Phase A (L1) | V |
+| `voltageL2` | Average RMS voltage on Phase B (L2) | V |
+| `voltageL3` | Average RMS voltage on Phase C (L3) | V |
+| `powerFactorL1` | Average percent IEC power factor of Phase A (L1) | % |
+| `powerFactorL2` | Average percent IEC power factor of Phase B (L2) | % |
+| `powerFactorL3` | Average percent IEC power factor of Phase C (L3) | % |
 | `capacitorVoltage` | The capacitor voltage at time of transmit. | V |
 
 ### Phase Amperage and Max Amperage (Packet #41)
 | Name | Description | Units |
 | --- | --- | :---: |
-| `currentL1` | Average RMS amps on Phase A (L1) since last transmit. | A |
-| `currentL2` | Average RMS amps on Phase B (L2) since last transmit. | A |
-| `currentL3` | Average RMS amps on Phase B (L3) since last transmit. | A |
+| `currentL1` | Average RMS amps on Phase A (L1). | A |
+| `currentL2` | Average RMS amps on Phase B (L2). | A |
+| `currentL3` | Average RMS amps on Phase C (L3). | A |
 | `maxCurrentL1` | Maximum amps on Phase A (L1) since last transmit. | A |
 | `maxCurrentL2` | Maximum amps on Phase B (L2) since last transmit. | A |
 | `maxCurrentL3` | Maximum amps on Phase C (L3) since last transmit. | A |
 | `temperatureCelsius` | The temperature at time of transmit. | °C |
 
 ### Active Energy Accumulation (Packets #42-43)
+In environments with energy generation (such as solar) the active energy accumulator may decrease and even go negative if the reverse energy flow exceeds forward consumption.
 | Name | Description | Units |
 | --- | --- | :---: |
-| `activeEnergyAccumulation` | The total active energy watt-hour accumulation. | Wh |
-| `averagePowerFactor` | The average power factor across all phases since last transmit. | % |
+| `activeEnergyAccumulation` | The total active energy watt-hour accumulation.  | Wh |
+| `averagePowerFactor` | The average power factor across all phases. | % |
 
 ### Apparent Energy Accumulation (Packets #44-45)
+The apparent energy accumulator is an absolute value sum of all energy passing through the meter and is strictly increasing, even in environments with reverse energy flow.
 | Name | Description | Units |
 | --- | --- | :---: |
 | `apparentEnergyAccumulation` | The total apparent energy volt-amp-hour accumulation. | VAh |
-| `averagePowerFactor` | The average power factor across all phases since last transmit. | % |
+| `averagePowerFactor` | The average power factor across all phases. | % |
 
 ### Startup Diagnostic (Packet #46)
 The startup diagnostic is sent the first time the system joins a LoRaWAN network after a reset has occurred.
@@ -96,6 +128,6 @@ The operational diagnostic is sent when certain error conditions occur. Dependin
 }
 ```
 
-For additional information on the decoded values and types for the VoltDrop direct meausrements [check the uplink.schema.json file](/voltdrop_direct/uplink.schema.json)
+For additional information on the decoded values and types for the VoltDrop direct measurements [check the uplink.schema.json file](/voltdrop_direct/uplink.schema.json)
 
 For additional information on downlinks, check the [base VoltDrop codec README](/voltdrop_direct/index-readme.md).
--- voltdrop_direct/chirpstack-v3/index.js
+++ voltdrop_direct/chirpstack-v3/index.js
@@ -14,6 +14,8 @@
  */
 function Decode(fPort, bytes, variables) {
   var packetList = {
+    VD_DIRECT_CONSOLIDATED_VOLTAGE_AMPERAGE: 38,
+    VD_DIRECT_VOLTAGE_ANGLE: 39,
     VD_DIRECT_VOLTAGE_PF: 40,
     VD_DIRECT_AMPERAGE: 41,
     VD_DIRECT_ACT_ENERGY_CONF: 42,
@@ -25,6 +27,7 @@ function Decode(fPort, bytes, variables) {
   };
 
   // Constant factors for formulas
+  var phaseAngleFactor = 360.0 / 255.0;
   var capacitorVoltageFactor = 5.0 / 255.0;
   var temperatureCelsiusFactor = 120.0 / 255.0;
 
@@ -33,14 +36,88 @@ function Decode(fPort, bytes, variables) {
   };
   var rawBytesArray = bytes;
 
-  // Uplink payload must be 11 bytes long.
-  if (rawBytesArray.length != 11) {
-    throw new Error("Payload length must be 11 bytes");
+  // Packet must minimally contain an ID byte
+  if (rawBytesArray.length != 0) {
+    var packetId = rawBytesArray[0];
+    switch (packetId) {
+      // The consolidated packet is larger than typical
+      case packetList.VD_DIRECT_CONSOLIDATED_VOLTAGE_AMPERAGE:
+        expectedLength = 21;
+        break;
+      default: // All other packets are currently 11 bytes long
+        expectedLength = 11;
+      break;
+    }
+
+    if (rawBytesArray.length != expectedLength) {
+      throw new Error("Invalid payload length for data type");
+    }
+  } else {
+    throw new Error("Empty payload");
   }
 
   // Packet ID - 1 byte
   var packetId = rawBytesArray[0];
   switch (packetId) {
+    case packetList.VD_DIRECT_CONSOLIDATED_VOLTAGE_AMPERAGE: {
+      var currentL1 =
+      (((rawBytesArray[11] << 8) + rawBytesArray[12]) >>> 0) / 16.0;
+      var currentL2 =
+        (((rawBytesArray[13] << 8) + rawBytesArray[14]) >>> 0) / 16.0;
+      var currentL3 =
+        (((rawBytesArray[15] << 8) + rawBytesArray[16]) >>> 0) / 16.0;
+      result.data = {
+        // Average Phase Voltages - 2 bytes each
+        // 16-bit unsigned integers in network byte order (MSB/BE) with 10 integer and 6 fractional bits
+        voltageL1: (((rawBytesArray[1] << 8) + rawBytesArray[2]) >>> 0) / 64.0,
+        voltageL2: (((rawBytesArray[3] << 8) + rawBytesArray[4]) >>> 0) / 64.0,
+        voltageL3: (((rawBytesArray[5] << 8) + rawBytesArray[6]) >>> 0) / 64.0,
+        // Phase Angle Scalars - 1 byte each
+        // 8-bit unsigned integer representing the phase angle between voltage and current.
+        // (as if the integer range from 0-255 is scaled to between 0° and 358.59375°)
+        phaseAngleL1: rawBytesArray[7] * phaseAngleFactor,
+        phaseAngleL2: rawBytesArray[8] * phaseAngleFactor,
+        phaseAngleL3: rawBytesArray[9] * phaseAngleFactor,
+        // Capacitor Voltage Scalar - 1 byte
+        // 8-bit unsigned integer representing the capacitor voltage.
+        // (as if the integer range from 0-255 is scaled to between 0.0V and 5.0V)
+        capacitorVoltage: rawBytesArray[10] * capacitorVoltageFactor,
+        // Average Phase Current - 2 bytes each
+        // 16-bit unsigned integers in network byte order (MSB/BE) with 12 integer and 4 fractional bits
+        currentL1: currentL1,
+        currentL2: currentL2,
+        currentL3: currentL3,
+        // Maximum Phase Current - 1 byte each
+        // 8-bit unsigned integer with 3 integer and 5 fractional bits (expressed as percentage in addition to 100%)
+        maxCurrentL1: (rawBytesArray[17] / 32.0 + 1.0) * currentL1,
+        maxCurrentL2: (rawBytesArray[18] / 32.0 + 1.0) * currentL2,
+        maxCurrentL3: (rawBytesArray[19] / 32.0 + 1.0) * currentL3,
+        // Temperature Scalar
+        // 8-bit unsigned integer representing the temperature.
+        // (as if the integer range from 0-255 is scaled to between -40C and 80C)
+        temperatureCelsius: rawBytesArray[20] * temperatureCelsiusFactor - 40.0,
+      };
+      break;
+    }
+    case packetList.VD_DIRECT_VOLTAGE_ANGLE:
+      result.data = {
+        // Average Phase Voltages - 2 bytes each
+        // 16-bit unsigned integers in network byte order (MSB/BE) with 10 integer and 6 fractional bits
+        voltageL1: (((rawBytesArray[1] << 8) + rawBytesArray[2]) >>> 0) / 64.0,
+        voltageL2: (((rawBytesArray[3] << 8) + rawBytesArray[4]) >>> 0) / 64.0,
+        voltageL3: (((rawBytesArray[5] << 8) + rawBytesArray[6]) >>> 0) / 64.0,
+        // Phase Angle Scalars - 1 byte each
+        // 8-bit unsigned integer representing the phase angle between voltage and current.
+        // (as if the integer range from 0-255 is scaled to between 0° and 358.59375°)
+        phaseAngleL1: rawBytesArray[7] * phaseAngleFactor,
+        phaseAngleL2: rawBytesArray[8] * phaseAngleFactor,
+        phaseAngleL3: rawBytesArray[9] * phaseAngleFactor,
+        // Capacitor Voltage Scalar - 1 byte
+        // 8-bit unsigned integer representing the capacitor voltage.
+        // (as if the integer range from 0-255 is scaled to between 0.0V and 5.0V)
+        capacitorVoltage: rawBytesArray[10] * capacitorVoltageFactor,
+      };
+      break;
     case packetList.VD_DIRECT_VOLTAGE_PF:
       result.data = {
         // Average Phase Voltages - 2 bytes each
@@ -84,24 +161,31 @@ function Decode(fPort, bytes, variables) {
       break;
     case packetList.VD_DIRECT_ACT_ENERGY_CONF: // Intentional fall-through
     case packetList.VD_DIRECT_ACT_ENERGY_UNCONF:
+      // Total Forward Active Energy - 8 bytes
+      // Sum of all phases active energy accumulated in Watt-Hours since last factory reset downlink.
+      // 64-bit signed integer in network byte order (MSB/BE)
+      //
+      // NOTE: Due to the limitations of Javascript and JSON this codec only uses 2^53 bits
+      // This still gives an effective range of 102,821 years at 10 Mega-Watts which is more than the
+      // Voltdrop is reasonably capable of measuring.
+      // Javascript cannot handle bit-shifts above 16 without truncation so they have been replaced by
+      // equivalent multipliers. rawBytesArray[1] is above the 53-bit limit and intentionally ignored.
+      let activeEnergyAccumulation =
+        ((rawBytesArray[2] & 0x1f) * Math.pow(2, 48) +
+        rawBytesArray[3] * Math.pow(2, 40) +
+        rawBytesArray[4] * Math.pow(2, 32) +
+        rawBytesArray[5] * Math.pow(2, 24) +
+        (rawBytesArray[6] << 16) +
+        (rawBytesArray[7] << 8) +
+        rawBytesArray[8]);
+      // At this point the value is considered unsigned but may contain a 2's compliment negative value.
+      // Check sign bit and subtract to make the floating-point representation negative if needed.
+      if (activeEnergyAccumulation >= Math.pow(2, 52)) {
+        activeEnergyAccumulation -= Math.pow(2, 53);
+      }
+
       result.data = {
-        // Total Forward Active Energy - 8 bytes
-        // Sum of all phases active energy accumulated in Watt-Hours since last factory reset downlink.
-        // 64-bit unsigned integer in network byte order (MSB/BE)
-        //
-        // NOTE: Due to the limitations of Javascript and JSON this codec only uses 2^53 bits
-        // This still gives an effective range of 102,821 years at 10 Mega-Watts which is more than the
-        // Voltdrop is reasonably capable of measuring.
-        // Javascript cannot handle bit-shifts above 16 without truncation so they have been replaced by
-        // equivalent multipliers. rawBytesArray[1] is above the 53-bit limit and intentionally ignored.
-        activeEnergyAccumulation:
-          (rawBytesArray[2] & 0x1f) * 281474976710656 +
-          rawBytesArray[3] * 1099511627776 +
-          rawBytesArray[4] * 4294967296 +
-          rawBytesArray[5] * 16777216 +
-          (rawBytesArray[6] << 16) +
-          (rawBytesArray[7] << 8) +
-          rawBytesArray[8],
+        activeEnergyAccumulation: activeEnergyAccumulation,
         // Average Power Factor over all Phases - 2 bytes
         // 16-bit signed integer in network byte order (MSB/BE) expressed as percentage with 8 integer and 7 fractional bits
         averagePowerFactor:
@@ -117,10 +201,10 @@ function Decode(fPort, bytes, variables) {
         //
         // NOTE: See info about activeEnergyAccumulation and 53 bit numeric limitation
         apparentEnergyAccumulation:
-          (rawBytesArray[2] & 0x1f) * 281474976710656 +
-          rawBytesArray[3] * 1099511627776 +
-          rawBytesArray[4] * 4294967296 +
-          rawBytesArray[5] * 16777216 +
+          (rawBytesArray[2] & 0x1f) * Math.pow(2, 48) +
+          rawBytesArray[3] * Math.pow(2, 40) +
+          rawBytesArray[4] * Math.pow(2, 32) +
+          rawBytesArray[5] * Math.pow(2, 24) +
           (rawBytesArray[6] << 16) +
           (rawBytesArray[7] << 8) +
           rawBytesArray[8],
@@ -200,6 +284,7 @@ function Decode(fPort, bytes, variables) {
         "Reader Overvoltage",
         "Reader Not Calibrated",
         "Phase Sequence Error",
+        "Transmit Duty-Cycle Restricted",
       ];
       var rawErrorConditions =
         ((rawBytesArray[1] << 8) + rawBytesArray[2]) >>> 0;
